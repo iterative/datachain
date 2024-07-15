0e3cf2827eeda0c3b18d8cf04ab09ee37d8ee605
skshetry <18718008+skshetry@users.noreply.github.com>
2024-07-15 22:04:23 +0545

    rollback #15 partially (#50)

--- tests/func/test_pytorch.py
+++ tests/func/test_pytorch.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import open_clip
 import pytest
 from torch import Size, Tensor
@@ -10,23 +8,18 @@ from datachain.lib.dc import DataChain
 from datachain.lib.pytorch import PytorchDataset
 
 
-@pytest.fixture(scope="module")
-def fake_dir(tmpdir_factory):
+@pytest.fixture
+def fake_dataset(catalog, tmp_path):
     # Create fake images in labeled dirs
-    data_path = Path(tmpdir_factory.mktemp("data"))
+    data_path = tmp_path / "data" / ""
     for i, (img, label) in enumerate(FakeData()):
         label = str(label)
         (data_path / label).mkdir(parents=True, exist_ok=True)
         img.save(data_path / label / f"{i}.jpg")
-
     # Create dataset from images
-    return data_path.as_uri()
-
-
-@pytest.fixture
-def fake_dataset(fake_dir):
+    uri = data_path.as_uri()
     return (
-        DataChain.from_storage(fake_dir, type="image")
+        DataChain.from_storage(uri, type="image")
         .map(text=lambda file: file.parent.split("/")[-1], output=str)
         .map(label=lambda text: int(text), output=int)
         .save("fake")
