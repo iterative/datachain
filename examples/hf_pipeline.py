637eeffa34f43119256da5e7c4a5c68d4af727d0
Dave Berenbaum <dave@iterative.ai>
2024-07-15 14:43:17 -0400

    update hf pipeline

--- examples/hf_pipeline.py
+++ examples/hf_pipeline.py
@@ -1,8 +1,9 @@
 # pip install torch
+# NOTE: also need to install ffmpeg binary
 import torch
 
-from datachain.lib.hf_pipeline import ImageHelper, RawHelper, TextHelper
-from datachain.query import C, DatasetQuery
+from datachain.lib.dc import C, DataChain
+from datachain.lib.hf_pipeline import Helper
 
 image_source = "gs://dvcx-datalakes/dogs-and-cats/"
 audio_source = "gs://dvcx-datalakes/speech-emotion-recognition-dataset/"
@@ -18,81 +19,89 @@ if __name__ == "__main__":
     print("** HuggingFace pipeline helper model zoo demo **")
     print("\nZero-shot object detection and classification:")
     results = (
-        DatasetQuery(
+        DataChain.from_storage(
             image_source,
             anon=True,
+            type="image",
         )
-        .filter(C.name.glob("*.jpg"))
+        .filter(C("name").glob("*.jpg"))
         .limit(1)
-        .add_signals(
-            ImageHelper(
+        .map(
+            Helper(
                 model="google/owlv2-base-patch16",
                 device=device,
                 candidate_labels=["cat", "dog", "squirrel", "unknown"],
             ),
-            parallel=False,
+            params=["file"],
+            output={"model_output": dict, "error": str},
         )
-        .select("source", "parent", "name", "model_output", "error")
+        .select("file.source", "file.parent", "file.name", "model_output", "error")
         .results()
     )
     print(*results, sep="\n")
 
     print("\nNot-safe-for-work image detection:")
     results = (
-        DatasetQuery(
+        DataChain.from_storage(
             image_source,
             anon=True,
+            type="image",
         )
-        .filter(C.name.glob("*.jpg"))
+        .filter(C("name").glob("*.jpg"))
         .limit(1)
-        .add_signals(
-            ImageHelper(
+        .map(
+            Helper(
                 model="Falconsai/nsfw_image_detection",
                 device=device,
             ),
-            parallel=False,
+            params=["file"],
+            output={"model_output": dict, "error": str},
         )
-        .select("source", "parent", "name", "model_output", "error")
+        .select("file.source", "file.parent", "file.name", "model_output", "error")
         .results()
     )
     print(*results, sep="\n")
 
     print("\nAudio emotion classification:")
     results = (
-        DatasetQuery(
+        DataChain.from_storage(
             audio_source,
             anon=True,
+            type="binary",
         )
-        .filter(C.name.glob("*.wav"))
+        .filter(C("name").glob("*.wav"))
         .limit(1)
-        .add_signals(
-            RawHelper(
+        .map(
+            Helper(
                 model="Krithika-p/my_awesome_emotions_model",
                 device=device,
             ),
-            parallel=False,
+            params=["file"],
+            output={"model_output": dict, "error": str},
         )
-        .select("source", "parent", "name", "model_output", "error")
+        .select("file.source", "file.parent", "file.name", "model_output", "error")
         .results()
     )
     print(*results, sep="\n")
     print("\nLong text summarization:")
     results = (
-        DatasetQuery(
+        DataChain.from_storage(
             text_source,
             anon=True,
+            type="text",
         )
-        .filter(C.name.glob("*.story"))
+        .filter(C("name").glob("*.story"))
         .limit(1)
-        .add_signals(
-            TextHelper(
+        .map(
+            Helper(
                 model="pszemraj/led-large-book-summary",
                 device=device,
                 max_length=150,
             ),
-            parallel=False,
+            params=["file"],
+            output={"model_output": dict, "error": str},
         )
-        .select("source", "parent", "name", "model_output", "error")
+        .select("file.source", "file.parent", "file.name", "model_output", "error")
         .results()
     )
     print(*results, sep="\n")
--- src/datachain/lib/hf_pipeline.py
+++ src/datachain/lib/hf_pipeline.py
@@ -2,97 +2,22 @@ import json
 
 from transformers import pipeline
 
-from datachain.query import Object, udf
-from datachain.sql.types import JSON, String
-
-try:
-    from PIL import (
-        Image,
-        UnidentifiedImageError,
-    )
-except ImportError as exc:
-    raise ImportError(
-        "Missing dependency Pillow for computer vision:\n"
-        "To install run:\n\n"
-        "  pip install 'datachain[cv]'\n"
-    ) from exc
+from datachain.lib.udf import Mapper
 
 
-def read_image(raw):
-    try:
-        img = Image.open(raw)
-    except UnidentifiedImageError:
-        return None
-    img.load()
-    return img.convert("RGB")
-
-
-def read_object(raw):
-    return raw.read()
-
-
-def read_text(raw):
-    return read_object(raw).decode("utf-8")
-
-
-@udf(
-    params=(Object(read_image),),  # Columns consumed by the UDF.
-    output={
-        "model_output": JSON,
-        "error": String,
-    },  # Signals being returned by the UDF.
-    method="image_processor",
-)
-class ImageHelper:
+class Helper(Mapper):
     def __init__(self, model, device, **kwargs):
-        self.helper = pipeline(model=model, device=device)
+        self.model = model
+        self.device = device
         self.kwargs = kwargs
 
-    def image_processor(self, imgs):
+    def setup(self):
+        self.helper = pipeline(model=self.model, device=self.device)
+
+    def process(self, file):
+        imgs = file.get_value()
         result = self.helper(
             imgs,
-            **self.kwargs,
-        )
-        return (json.dumps(result), "")
-
-
-@udf(
-    params=(Object(read_text),),  # Columns consumed by the UDF.
-    output={
-        "model_output": JSON,
-        "error": String,
-    },  # Signals being returned by the UDF.
-    method="text_processor",
-)
-class TextHelper:
-    def __init__(self, model, device, **kwargs):
-        self.helper = pipeline(model=model, device=device)
-        self.kwargs = kwargs
-
-    def text_processor(self, text):
-        result = self.helper(
-            text,
-            **self.kwargs,
-        )
-        return (json.dumps(result), "")
-
-
-@udf(
-    params=(Object(read_object),),  # Columns consumed by the UDF.
-    output={
-        "model_output": JSON,
-        "error": String,
-    },  # Signals being returned by the UDF.
-    method="raw_processor",
-)
-class RawHelper:
-    def __init__(self, model, device, **kwargs):
-        self.helper = pipeline(model=model, device=device)
-        self.kwargs = kwargs
-
-    def raw_processor(self, obj):
-        result = self.helper(
-            obj,
             **self.kwargs,
         )
         return (json.dumps(result), "")
