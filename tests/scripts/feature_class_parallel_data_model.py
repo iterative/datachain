24f71cf2fa6d7a86a8db51e29dc7da42a807810c
Ronan Lamy <ronan.lamy@gmail.com>
2024-07-22 15:41:39 +0100

    Fix query E2E tests, remove obsolete script

--- tests/scripts/feature_class_parallel_data_model.py
+++ tests/scripts/feature_class_parallel_data_model.py
@@ -17,7 +17,7 @@ class Embedding(DataModel):
 ds_name = "feature_class"
 ds = (
     DataChain.from_storage("gs://dvcx-datalakes/dogs-and-cats/")
-    .filter(C.name.glob("*cat*.jpg"))  # type: ignore [attr-defined]
+    .filter(C.path.glob("*cat*.jpg"))  # type: ignore [attr-defined]
     .limit(5)
     .settings(cache=True, parallel=2)
     .map(emd=lambda file: Embedding(value=512), output=Embedding)
@@ -25,4 +25,4 @@ ds = (
 )
 
 for row in ds.results():
-    print(row[2])
+    print(row[1])
