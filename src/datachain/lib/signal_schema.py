93747e467b0cf9a0a98b9c2ec79e814a104c0adf
Matt Seddon <mattseddon@hotmail.com>
2024-08-01 13:36:22 +1000

    fixup: apply review feedback

--- src/datachain/lib/signal_schema.py
+++ src/datachain/lib/signal_schema.py
@@ -243,9 +243,9 @@ class SignalSchema:
         curr_type = None
         i = 0
         while curr_tree is not None and i < len(path):
-            if val := curr_tree.get(path[i], None):
+            if val := curr_tree.get(path[i]):
                 curr_type, curr_tree = val
-            elif i == 0 and (val := curr_tree.get(".".join(path), None)):
+            elif i == 0 and len(path) > 1 and (val := curr_tree.get(".".join(path))):
                 curr_type, curr_tree = val
                 break
             else:
