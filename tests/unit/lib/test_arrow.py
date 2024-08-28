6f2bd55c88de0222b7f4511e132ade5c867f0b86
Dave Berenbaum <dave@iterative.ai>
2024-08-28 16:03:24 -0400

    hf filesystem

--- tests/unit/lib/test_arrow.py
+++ tests/unit/lib/test_arrow.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import pandas as pd
 import pyarrow as pa
+import pyarrow.parquet as pq
 import pytest
 
 from datachain.lib.arrow import (
@@ -10,6 +11,7 @@ from datachain.lib.arrow import (
     arrow_type_mapper,
     schema_to_output,
 )
+from datachain.lib.data_model import dict_to_data_model
 from datachain.lib.file import File, IndexedFile
 
 
@@ -55,6 +57,34 @@ def test_arrow_generator_no_source(tmp_path, catalog):
         assert o[1] == text
 
 
+def test_arrow_generator_output_schema(tmp_path, catalog):
+    ids = [12345, 67890, 34, 0xF0123]
+    texts = ["28", "22", "we", "hello world"]
+    dicts = [{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}, {"a": 7, "b": 8}]
+    df = pd.DataFrame({"id": ids, "text": texts, "dict": dicts})
+    table = pa.Table.from_pandas(df)
+
+    name = "111.parquet"
+    pq_path = tmp_path / name
+    pq.write_table(table, pq_path)
+    stream = File(path=pq_path.as_posix(), source="file:///")
+    stream._set_stream(catalog, caching_enabled=False)
+
+    output_schema = dict_to_data_model("", schema_to_output(table.schema))
+    func = ArrowGenerator(output_schema=output_schema)
+    objs = list(func.process(stream))
+
+    assert len(objs) == len(ids)
+    for index, (o, id, text, dict) in enumerate(zip(objs, ids, texts, dicts)):
+        assert isinstance(o[0], IndexedFile)
+        assert isinstance(o[0].file, File)
+        assert o[0].index == index
+        assert o[1].id == id
+        assert o[1].text == text
+        assert o[1].dict.a == dict["a"]
+        assert o[1].dict.b == dict["b"]
+
+
 @pytest.mark.parametrize(
     "col_type,expected",
     (
@@ -72,7 +102,6 @@ def test_arrow_generator_no_source(tmp_path, catalog):
         (pa.date32(), datetime),
         (pa.string(), str),
         (pa.large_string(), str),
-        (pa.struct({"x": pa.int32(), "y": pa.string()}), dict),
         (pa.map_(pa.string(), pa.int32()), dict),
         (pa.dictionary(pa.int64(), pa.string()), str),
         (pa.list_(pa.string()), list[str]),
@@ -80,6 +109,14 @@ def test_arrow_generator_no_source(tmp_path, catalog):
 )
 def test_arrow_type_mapper(col_type, expected):
     assert arrow_type_mapper(col_type) == expected
+
+
+def test_arrow_type_mapper_struct():
+    col_type = pa.struct({"x": pa.int32(), "y": pa.string()})
+    fields = arrow_type_mapper(col_type).model_fields
+    assert list(fields.keys()) == ["x", "y"]
+    dtypes = [field.annotation for field in fields.values()]
+    assert dtypes == [int, str]
 
 
 def test_arrow_type_error():
