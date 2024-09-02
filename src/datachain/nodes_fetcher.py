3b7457387006f9be5b9bce0cf1cff0db589ce95a
Mandlin Sarah <mandlinsarah@gmail.com>
2024-09-02 05:18:33 -0700

    Improved logging and code readability in NodesFetcher

--- src/datachain/nodes_fetcher.py
+++ src/datachain/nodes_fetcher.py
@@ -4,16 +4,19 @@ from datachain.nodes_thread_pool import NodesThreadPool
 
 logger = logging.getLogger("datachain")
 
-
 class NodesFetcher(NodesThreadPool):
     def __init__(self, client, max_threads, cache):
         super().__init__(max_threads)
         self.client = client
         self.cache = cache
+        logger.info("NodesFetcher initialized with max_threads: %d", max_threads)
 
     def done_task(self, done):
         for task in done:
-            task.result()
+            try:
+                task.result()
+            except Exception as e:
+                logger.error("Task resulted in an error: %s", e)
 
     def do_task(self, chunk):
         from fsspec import Callback
@@ -25,6 +28,9 @@ class NodesFetcher(NodesThreadPool):
         for node in chunk:
             uid = node.as_uid(self.client.uri)
             if self.cache.contains(uid):
+                logger.debug("Cache hit for UID: %s", uid)
                 self.increase_counter(node.size)
             else:
+                logger.debug("Cache miss for UID: %s, adding to cache", uid)
                 self.client.put_in_cache(uid, callback=_CB())
+
