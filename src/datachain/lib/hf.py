81064de93a8d9952b5ca7fb801ca5aa5ba3fa3f9
Matt Seddon <mattseddon@hotmail.com>
2024-09-05 13:38:43 +1000

    fix stream splits lint issue

--- src/datachain/lib/hf.py
+++ src/datachain/lib/hf.py
@@ -99,7 +99,9 @@ class HFGenerator(Generator):
 
 def stream_splits(ds: Union[str, HFDatasetType], *args, **kwargs):
     if isinstance(ds, str):
-        ds = load_dataset(ds, *args, streaming=True, **kwargs)
+        if "streaming" not in kwargs:
+            kwargs["streaming"] = True
+        ds = load_dataset(ds, *args, **kwargs)
     if isinstance(ds, (DatasetDict, IterableDatasetDict)):
         return ds
     return {"": ds}
