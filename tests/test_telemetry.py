a3e8e4593dd91c59a857ad8e0803017b37ae580d
Amrit Ghimire <mail@amritghimire.com>
2024-09-09 20:11:19 +0545

    Env

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -1,5 +1,3 @@
-import os
-
 from datachain.lib.dc import DataChain
 from datachain.telemetry import is_enabled
 
@@ -10,7 +8,6 @@ def test_is_enabled():
 
 def test_telemetry_api_call(mocker, tmp_dir):
     patch_send = mocker.patch("iterative_telemetry.IterativeTelemetryLogger.send")
-    os.environ["DATACHAIN_TEST"] = "true"
 
     DataChain.from_storage(tmp_dir.as_uri())
     assert patch_send.call_count == 1
