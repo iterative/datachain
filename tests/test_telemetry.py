c5649162959973f454bc1fc8e3454d5e41ed4bae
Amrit Ghimire <mail@amritghimire.com>
2024-09-11 00:13:10 +0545

    Reset telemetry sent

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -1,13 +1,14 @@
+from datachain import telemetry
 from datachain.lib.dc import DataChain
-from datachain.telemetry import is_enabled
 
 
 def test_is_enabled():
-    assert not is_enabled()
+    assert not telemetry.is_enabled()
 
 
 def test_telemetry_api_call(mocker, tmp_dir):
     patch_send = mocker.patch("iterative_telemetry.IterativeTelemetryLogger.send")
+    telemetry._telemetry_sent = False
 
     DataChain.from_storage(tmp_dir.as_uri())
     assert patch_send.call_count == 1
