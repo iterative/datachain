f8f3ea40eebf4151dd8a8d4a1e1fc8fd6bff02d4
Amrit Ghimire <mail@amritghimire.com>
2024-09-18 06:33:32 +0545

    Fix tests

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -1,5 +1,5 @@
-from datachain import telemetry
 from datachain.lib.dc import DataChain
+from datachain.telemetry import telemetry
 
 
 def test_is_enabled():
@@ -8,7 +8,7 @@ def test_is_enabled():
 
 def test_telemetry_api_call(mocker, tmp_dir):
     patch_send = mocker.patch("iterative_telemetry.IterativeTelemetryLogger.send")
-    telemetry._telemetry_sent = False
+    telemetry._event_sent = False
 
     DataChain.from_storage(tmp_dir.as_uri())
     assert patch_send.call_count == 1
