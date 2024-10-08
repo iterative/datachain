74c4b01527be3cd2f68e46eff5c7721331a181c0
Amrit Ghimire <mail@amritghimire.com>
2024-10-08 19:57:31 +0545

    Simplify test for atomicity (#501)
    https://github.com/iterative/datachain/pull/494#discussion_r1791039347

--- tests/scripts/feature_class_exception.py
+++ tests/scripts/feature_class_exception.py
@@ -1,24 +1,11 @@
-# Set logger to debug level
 import logging
 
-from pydantic import BaseModel
-
-from datachain.lib.dc import C, DataChain
+from datachain.lib.dc import DataChain
 
 logging.basicConfig(level=logging.INFO)
 
 
-class Embedding(BaseModel):
-    value: float
-
-
 ds_name = "feature_class_error"
-ds = (
-    DataChain.from_storage("gs://dvcx-datalakes/dogs-and-cats/")
-    .filter(C("file.path").glob("*cat*.jpg"))
-    .limit(5)
-    .map(emd=lambda file: Embedding(value=512), output=Embedding)
-)
-ds.select("file.path", "emd.value").show(limit=5, flatten=True)
+ds = DataChain.from_values(key=["a", "b", "c"])
 ds.save(ds_name)
 raise Exception("This is a test exception")
