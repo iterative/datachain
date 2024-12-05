c7c5d220fb7eefad00293f3262f99c208d50d451
Vladimir Rudnykh <dreadatour@gmail.com>
2024-12-05 12:56:47 +0700

    Update src/datachain/toolkit/split.py
    Co-authored-by: Amrit Ghimire <16842655+amritghimire@users.noreply.github.com>

--- src/datachain/toolkit/split.py
+++ src/datachain/toolkit/split.py
@@ -68,10 +68,7 @@ def train_test_split(
         raise ValueError("Weights should be non-negative")
 
     def scale(multiplier: float) -> int:
-        if multiplier < 0:
-            multiplier = 0.0
-        if multiplier > 1:
-            multiplier = 1.0
+        multiplier = min(max(0, multiplier),1.0)
         numerator, denominator = float(multiplier).as_integer_ratio()
         return MAX_SIGNED_INT64 * numerator // denominator
 
