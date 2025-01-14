89ee2f0f15ce225a62635d8bfeaf62b81ac74695
skshetry <18718008+skshetry@users.noreply.github.com>
2025-01-14 10:35:09 +0545

    progress: remove unused logging/tqdm lock (#817)

--- src/datachain/progress.py
+++ src/datachain/progress.py
@@ -1,14 +1,5 @@
-"""Manages progress bars."""
-
-import logging
-from threading import RLock
-
 from fsspec import Callback
 from fsspec.callbacks import TqdmCallback
-from tqdm.auto import tqdm
-
-logger = logging.getLogger(__name__)
-tqdm.set_lock(RLock())
 
 
 class CombinedDownloadCallback(Callback):
@@ -24,10 +15,6 @@ class CombinedDownloadCallback(Callback):
 class TqdmCombinedDownloadCallback(CombinedDownloadCallback, TqdmCallback):
     def __init__(self, tqdm_kwargs=None, *args, **kwargs):
         self.files_count = 0
-        tqdm_kwargs = tqdm_kwargs or {}
-        tqdm_kwargs.setdefault("postfix", {}).setdefault("files", self.files_count)
-        kwargs = kwargs or {}
-        kwargs["tqdm_cls"] = tqdm
         super().__init__(tqdm_kwargs, *args, **kwargs)
 
     def increment_file_count(self, n: int = 1) -> None:
