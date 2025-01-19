562177da4fd22a7984e036c89b84fb8fec31c05a
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2025-01-18 16:38:26 -0800

    python_to_sql tests to separate file

--- tests/unit/lib/test_utils.py
+++ tests/unit/lib/test_utils.py
@@ -1,12 +1,12 @@
-from collections.abc import Iterable, Mapping
-from typing import Literal, Optional, Union
+from collections.abc import Iterable
+from typing import Union
 
 import pytest
 from pydantic import BaseModel
 
 from datachain.lib.convert.python_to_sql import python_to_sql
 from datachain.lib.utils import normalize_col_names
-from datachain.sql.types import JSON, Array, Float, String
+from datachain.sql.types import Array, String
 
 
 class MyModel(BaseModel):
@@ -15,43 +15,6 @@ class MyModel(BaseModel):
 
 class MyFeature(BaseModel):
     val1: str
-
-
-@pytest.mark.parametrize(
-    "typ,expected",
-    (
-        (str, String),
-        (String, String),
-        (Literal["text"], String),
-        (dict[str, int], JSON),
-        (Mapping[str, int], JSON),
-        (Optional[str], String),
-        (Union[dict, list[dict]], JSON),
-    ),
-)
-def test_convert_type_to_datachain(typ, expected):
-    assert python_to_sql(typ) == expected
-
-
-def test_list_of_tuples_matching_types():
-    assert (
-        python_to_sql(list[tuple[float, float]]).to_dict()
-        == Array(Array(Float)).to_dict()
-    )
-
-
-def test_list_of_tuples_object():
-    assert (
-        python_to_sql(list[tuple[float, MyModel]]).to_dict()
-        == Array(Array(JSON)).to_dict()
-    )
-
-
-def test_list_of_tuples_not_matching_types():
-    assert (
-        python_to_sql(list[tuple[float, String]]).to_dict()
-        == Array(Array(JSON)).to_dict()
-    )
 
 
 @pytest.mark.parametrize(
