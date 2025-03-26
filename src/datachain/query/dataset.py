e3453b06ec3321d8e829ac147dcdeda794c7d67e
Vladimir Rudnyh <dreadatour@gmail.com>
2025-03-26 20:15:03 +0700

    Pass  to UDF distributor to do not fetch it twice

--- src/datachain/query/dataset.py
+++ src/datachain/query/dataset.py
@@ -472,6 +472,7 @@ class UDFStep(Step, ABC):
                         use_cache=self.cache,
                         is_generator=self.is_generator,
                         min_task_size=self.min_task_size,
+                        rows_total=rows_total,
                     )
                     udf_distributor()
                 elif processes:
