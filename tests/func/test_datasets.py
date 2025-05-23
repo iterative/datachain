9b0786feeb69b24f72b6537875648e9597e219a7
ilongin <ivan.longin1@gmail.com>
2025-05-23 14:37:45 +0200

    adding TODO

--- tests/func/test_datasets.py
+++ tests/func/test_datasets.py
@@ -97,7 +97,8 @@ def test_create_dataset_with_explicit_version(cloud_test_catalog, project, creat
 
 @pytest.mark.parametrize("create_rows", [True, False])
 def test_create_dataset_already_exist_but_in_different_project():
-    pass
+    # TODO finish test
+    assert 1 == 2
 
 
 @pytest.mark.parametrize("create_rows", [True, False])
