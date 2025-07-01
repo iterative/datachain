ff89d45372ae86b5a06a0222aafffb6749fae218
Ivan Shcheklein <shcheklein@gmail.com>
2025-06-30 18:02:36 -0700

    fix computer vision examples

--- examples/computer_vision/llava2_image_desc_lib.py
+++ examples/computer_vision/llava2_image_desc_lib.py
@@ -12,6 +12,7 @@ from transformers import (
 )
 
 import datachain as dc
+from datachain import C, File
 
 model = "llava-hf/llava-1.5-7b-hf"
 
@@ -23,64 +24,48 @@ model = "llava-hf/llava-1.5-7b-hf"
 #
 # https://huggingface.co/llava-hf
 
-source = "gs://datachain-demo/dogs-and-cats/"
 
-# device='mps' not supported
-if torch.cuda.is_available():
-    device = "cuda"
-else:
-    device = "cpu"
+# Probably this code can be written with HF pipeline
+# but we keep it a bit more low-level for the sake of example.
+class LLaVaProcessor:
+    def __init__(self, model_name, max_tokens=300):
+        if torch.cuda.is_available():
+            self.device = "cuda"
+            self.dtype = torch.float16
+        else:
+            self.device = "cpu"
+            self.dtype = torch.float32
 
-
-DEFAULT_FIT_BOX = (500, 500)
-
-
-def infer_dtype(device):
-    if device == "cpu":
-        return torch.float32
-    return torch.float16
-
-
-class LLaVADescribe(dc.Mapper):
-    def __init__(self, device="cpu", model="llava-hf/llava-1.5-7b-hf", max_tokens=300):
-        self.device = device
-        self.model_name = model
+        self.model_name = model_name
         self.max_tokens = max_tokens
         self.prompt = "USER: <image>\nDescribe this picture\nASSISTANT:"
 
-    def setup(self):
-        self.torch_dtype = infer_dtype(self.device)
         self.processor = AutoProcessor.from_pretrained(self.model_name)
         self.model = LlavaForConditionalGeneration.from_pretrained(
-            self.model_name, torch_dtype=self.torch_dtype, low_cpu_mem_usage=True
-        )
-        self.model.to(self.device)
+            self.model_name, torch_dtype=self.dtype, low_cpu_mem_usage=True
+        ).to(self.device)
 
-    def process(self, file):
-        inputs = self.processor(
-            text=self.prompt, images=file.read(), return_tensors="pt"
-        ).to(self.device, self.torch_dtype)
 
-        generated_ids = self.model.generate(**inputs, max_new_tokens=self.max_tokens)
-        generated_text = self.processor.batch_decode(
-            generated_ids, skip_special_tokens=True
-        )
-        desc = generated_text[0]
-        return desc.split("ASSISTANT:")[-1].strip(), ""
+def process(processor: LLaVaProcessor, file: File) -> tuple[str, str]:
+    inputs = processor.processor(
+        text=processor.prompt, images=file.read(), return_tensors="pt"
+    ).to(processor.device, processor.dtype)
+
+    generated_ids = processor.model.generate(
+        **inputs, max_new_tokens=processor.max_tokens
+    )
+    generated_text = processor.processor.batch_decode(
+        generated_ids, skip_special_tokens=True
+    )
+    desc = generated_text[0]
+    return desc.split("ASSISTANT:")[-1].strip(), ""
 
 
 if __name__ == "__main__":
     (
-        dc.read_storage(source, type="image")
-        .filter(dc.C("file.path").glob("*/cat*.jpg"))
-        .map(
-            desc=LLaVADescribe(
-                device=device,
-                model=model,
-            ),
-            params=["file"],
-            output={"description": str, "error": str},
-        )
-        .select("file.source", "file.path", "desc.description", "desc.error")
-        .show(2)
+        dc.read_storage("gs://datachain-demo/dogs-and-cats/", type="image", anon=True)
+        .filter(C("file.path").glob("*/cat*.jpg"))
+        .setup(processor=lambda: LLaVaProcessor(model_name=model))
+        .map(process, output=("description", "error"))
+        .show(5)
     )
