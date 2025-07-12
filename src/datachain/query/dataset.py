ae8025ec2d66b8de6e9c0f35629ac1429efa355c
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2025-07-11 17:17:06 -0700

    linter

--- src/datachain/query/dataset.py
+++ src/datachain/query/dataset.py
@@ -1031,14 +1031,18 @@ class SQLGroupBy(SQLClause):
             c.get_column() if isinstance(c, Function) else c for c in self.group_by
         ]
 
-        cols_dict = {
-            c.name: c.get_column()
-            if isinstance(c, Function)
-            else subquery.c[str(c)]
-            if isinstance(c, (str, C))
-            else c
-            for c in (*group_by, *self.cols)
-        }
+        cols_dict: dict[str, Any] = {}
+        for c in (*group_by, *self.cols):
+            if isinstance(c, Function):
+                key = c.name
+                value = c.get_column()
+            elif isinstance(c, (str, C)):
+                key = str(c)
+                value = subquery.c[str(c)]
+            else:
+                key = c.name
+                value = c  # type: ignore[assignment]
+            cols_dict[key] = value
 
         unique_cols = cols_dict.values()
 
