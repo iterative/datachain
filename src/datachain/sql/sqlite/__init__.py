d8708f2a4e3304b81b70334100d03ed13b96907e
Ivan Shcheklein <shcheklein@gmail.com>
2025-08-15 19:12:38 -0700

    remove adapters_registered that was used to debug

--- src/datachain/sql/sqlite/__init__.py
+++ src/datachain/sql/sqlite/__init__.py
@@ -1,12 +1,10 @@
 from .base import (
-    adapters_registered,
     create_user_defined_sql_functions,
     setup,
     sqlite_dialect,
 )
 
 __all__ = [
-    "adapters_registered",
     "create_user_defined_sql_functions",
     "setup",
     "sqlite_dialect",
