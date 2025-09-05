9d93604f3907960c4bcd82edc632e95e336d0749
ilongin <ivan.longin1@gmail.com>
2025-09-05 14:08:31 +0200

    making delete namespace public

--- src/datachain/__init__.py
+++ src/datachain/__init__.py
@@ -37,6 +37,7 @@ from datachain.lib.file import (
     VideoFrame,
 )
 from datachain.lib.model_store import ModelStore
+from datachain.lib.namespaces import delete as delete_namespace
 from datachain.lib.projects import create as create_project
 from datachain.lib.projects import delete as delete_project
 from datachain.lib.udf import Aggregator, Generator, Mapper
@@ -75,6 +76,7 @@ __all__ = [
     "create_project",
     "datasets",
     "delete_dataset",
+    "delete_namespace",
     "delete_project",
     "is_chain_type",
     "is_studio",
