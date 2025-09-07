be6eeaf4a882db82fa051b2384f5eeeb8bef97f3
Ivan Shcheklein <shcheklein@gmail.com>
2025-09-07 10:36:11 -0700

    fix(ds query): isolate temp table names

--- src/datachain/query/dataset.py
+++ src/datachain/query/dataset.py
@@ -1423,6 +1423,7 @@ class DatasetQuery:
         obj.steps = obj.steps.copy()
         if new_table:
             obj.table = self.get_table()
+        obj.temp_table_names = []
         return obj
 
     @detach
@@ -1766,7 +1767,7 @@ class DatasetQuery:
             query = self.clone()
             query.apply_steps()
         finally:
-            self.cleanup()
+            query.cleanup()
         return query
 
     def save(
