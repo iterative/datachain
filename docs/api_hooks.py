6c1bdaea016dbac4f8f7bd1a077979a6d918e643
Amrit Ghimire <16842655+amritghimire@users.noreply.github.com>
2025-09-10 20:06:34 +0545

    Add documentation page for API (#1316)

---
+++ docs/api_hooks.py
@@ -0,0 +1,35 @@
+# ruff: noqa: INP001
+def on_pre_build(**kwargs):
+    import os
+
+    import requests
+
+    # Download OpenAPI spec
+    response = requests.get(
+        "https://studio.datachain.ai/api/openapi.json",
+        timeout=30,
+    )
+
+    # Write to file
+    print("Writing OpenAPI spec to docs/openapi.json")
+    with open("docs/openapi.json", "w") as f:
+        f.write(response.text)
+
+    # Generate API docs using widdershins
+    print("Generating API docs using widdershins")
+    cmd = [
+        "npx widdershins",
+        "docs/openapi.json",
+        "-o docs/studio/api/index.md",
+        "--language_tabs 'python:Python'",
+        "--language_tabs 'shell:curl'",
+        "--expandBody true",
+        "--summary",
+        "--shallowSchemas",
+        "--omitBody",
+        "--resolve",
+        "--httpsnippet",
+        "-u docs/templates",
+        "--omitHeader",
+    ]
+    os.system(" ".join(cmd))  # noqa: S605
